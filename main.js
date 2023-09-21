f1038d516fcf78c218b9141e9b5d2ed8fd92871b
liviabeatrizml <liviabeatrizmaia7@gmail.com>
2023-09-21 09:35:06 -0300

    Organizando o código fonte e descartando variáveis sem uso

--- main.js
+++ main.js
@@ -22,14 +22,14 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 //Creation of the moon and change of material
-const geometry = new THREE.SphereGeometry(2, 60, 60);
+const moonGeometry = new THREE.SphereGeometry(2, 60, 60);
 const moonTexture = new THREE.TextureLoader().load("images/moon_map.jpg");
-const material = new THREE.MeshPhongMaterial();
-material.map = moonTexture;
-const moon = new THREE.Mesh(geometry, material);
+const moonMaterial = new THREE.MeshPhongMaterial();
+moonMaterial.map = moonTexture;
+const moon = new THREE.Mesh(moonGeometry, moonMaterial);
 scene.add(moon);
 
-//Creation of the astronaut
+//Creation of the astronaut 3D model
 var astronaut;
 const loader = new GLTFLoader();
 loader.load('models/cosmonaut.glb',
@@ -62,17 +62,17 @@ class sky {
 //Creation the stars
 var quantStars = [];
 for (var i = 0; i < 100; i++) {
-    const geometry2 = new THREE.OctahedronGeometry(0.1, 0);
+    const starGeometry = new THREE.OctahedronGeometry(0.1, 0);
     var paint = getRandomColor();
-    const material2 = new THREE.MeshStandardMaterial({ color: paint, wireframe: false });
-    const star = new THREE.Mesh(geometry2, material2);
+    const starMaterial = new THREE.MeshStandardMaterial({ color: paint, wireframe: false });
+    const star = new THREE.Mesh(starGeometry, starMaterial);
     scene.add(star);
 
-    const obj1 = new sky(star);
-    quantStars.push(obj1)
+    const stars = new sky(star);
+    quantStars.push(stars)
 }
 
-//Light
+//Lights
 const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
 directionalLight.position.set(-1, 0, 0);
 scene.add(directionalLight);
@@ -81,35 +81,29 @@ const pointLight = new THREE.PointLight(0xffffff, 10000, 10000);
 pointLight.position.set(150, 100, -900);
 scene.add(pointLight);
 
-
 //Camera position
 camera.position.z = 20;
 
-//Boundary variables and speed
+//Variables
 var velX = 0.01;
 var velY = 0.07;
-var limitMaxX = 10.0;
-var limitMinX = -10.0;
-var limitMaxY = 15.0;
-var limitMinY = -15.0;
+var limitX = 10.0;
+var limitY = 15.0;
 var vel = 0.11;
-var passo = 0;
-var i = 0;
-var astronaut = 0;
-var angulo = 0;
+var angle = 0;
 
 function animate() {
     requestAnimationFrame(animate);
     //Starting the moon on the left
-    moon.position.x = -15 * (Math.cos(passo));
-    moon.position.y = 6 * Math.sin(passo);
+    moon.position.x = -15 * (Math.cos(angle));
+    moon.position.y = 6 * Math.sin(angle);
 
     //Limiting the area of the moon (arc)
-    if (moon.position.x > limitMaxX || moon.position.x < limitMinX) {
+    if (moon.position.x > limitX || moon.position.x < -limitX) {
         velX = -velX;
     }
 
-    if (moon.position.y > limitMaxY || moon.position.y < limitMinY) {
+    if (moon.position.y > limitY || moon.position.y < -limitY) {
         velY = -velY;
     }
 
@@ -122,13 +116,16 @@ animate();
 document.onkeydown = function (event) {
     console.log(event);
     if (event.key == "ArrowRight") {
-        passo += vel;
+        angle += vel;
+    }
+
+    if (event.key == " ") {
     }
 }
 
 document.onkeyup = function (event) {
     console.log(event);
     if (event.key == " ") {
-
+        
     }
 }
