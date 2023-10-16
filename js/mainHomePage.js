082bc0feedf0189ed075a19a46a5452bd2ac8b30
Geisa-mg <geisamorais8@gmail.com>
2023-10-16 13:20:13 -0300

    Últimas atualizações

--- js/mainHomePage.js
+++ js/mainHomePage.js
@@ -7,7 +7,7 @@ const scene = new THREE.Scene();
 //Camera
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.z = 1;
-camera.rotation.x = Math.PI/2;
+camera.rotation.x = Math.PI / 2;
 
 //Render
 const renderer = new THREE.WebGLRenderer();
@@ -18,13 +18,13 @@ document.body.appendChild(renderer.domElement);
 starGeometry = new THREE.Geometry();
 for (let i = 0; i < 6000; i++) {
     star = new THREE.Vector3(
-        Math.random() * 600 - 300, 
-        Math.random() * 600 - 300, 
+        Math.random() * 600 - 300,
+        Math.random() * 600 - 300,
         Math.random() * 600 - 300);
-    
-        star.vel = 0;
-        star.accel = 0.02;
-        starGeometry.vertices.push(star);
+
+    star.vel = 0;
+    star.accel = 0.02;
+    starGeometry.vertices.push(star);
 }
 
 let starTexture = new THREE.TextureLoader().load('images/star.png');
@@ -42,7 +42,7 @@ function animate() {
     starGeometry.vertices.forEach(starPoint => {
         starPoint.vel += starPoint.accel;
         starPoint.y -= starPoint.vel;
-        if(starPoint.y <-200){
+        if (starPoint.y < -200) {
             starPoint.y = 200;
             starPoint.vel = 0;
         }
